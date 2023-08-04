f88bc69b5a56d2cb701fe49fd158b4cc413b5399
Hilko Bengen <bengen@hilluzination.de>
2023-08-04 14:55:01 +0200

    Improve on library API
    - use builder pattern
    - treat generated code as an implementation detail
    - Support multiple queries
    - CLI: Implement fetch

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,10 +1,9 @@
+use std::io::Write;
 use std::path::PathBuf;
-
-use futures::stream::StreamExt;
 
 use clap::Parser;
 
-use velociraptor_api::{ApiClient, VqlCollectorArgs, VqlEnv, VqlRequest};
+use velociraptor_api::{Client, ClientConfig, QueryOptions};
 
 #[derive(Parser, Debug, Clone)]
 #[clap(version, about)]
@@ -20,6 +19,7 @@ struct Cli {
 enum SubCommand {
     /// Issue a server side VQL query
     Query(QueryCmd),
+    Fetch(FetchCmd),
 }
 
 #[derive(clap::Args, Clone, Debug)]
@@ -32,13 +32,13 @@ struct QueryCmd {
     env: Vec<(String, String)>,
     /// The query to run
     #[clap(value_parser)]
-    query: String,
+    query: Vec<String>,
 }
 
 #[derive(clap::Args, Clone, Debug)]
 struct FetchCmd {
-    #[clap(value_parser)]
-    client: String,
+    #[clap(long)]
+    output_file: PathBuf,
     #[clap(value_parser)]
     path: PathBuf,
 }
@@ -59,62 +59,31 @@ where
     Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
 }
 
-use tonic::transport::Channel;
-
-async fn do_query(
-    client: &mut ApiClient<Channel>,
-    cmd: &QueryCmd,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let org_id = cmd.org.clone().unwrap_or_else(|| "root".into());
-    let env = cmd
-        .env
-        .iter()
-        .cloned()
-        .map(|(key, value)| VqlEnv { key, value })
-        .collect::<Vec<_>>();
-
-    let request = tonic::Request::new(VqlCollectorArgs {
-        env,
-        org_id,
-        max_row: 10,
-        query: vec![VqlRequest {
-            name: "Test?!".into(),
-            vql: cmd.query.to_string(),
-        }],
-        ..VqlCollectorArgs::default()
-    });
-
-    let mut response = client.query(request).await?.into_inner();
-
-    let mut result: Vec<serde_json::Value> = vec![];
-    while let Some(Ok(msg)) = response.next().await {
-        if !msg.response.is_empty() {
-            let mut rows: Vec<serde_json::Value> = serde_json::from_str(&msg.response)?;
-            result.append(&mut rows);
-        }
-        if !msg.log.is_empty() {
-            print!("Log: {}", msg.log);
-        }
-    }
-
-    println!("Result:\n\n{}", serde_json::to_string_pretty(&result)?);
-
-    Ok(())
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
-    let channel = velociraptor_api::Config::from_yaml_file(cli.config.clone())?
-        .endpoint()?
-        .connect()
-        .await?;
+    let client = Client::try_from(&ClientConfig::from_yaml_file(cli.config.clone())?)?;
 
-    let mut client = ApiClient::new(channel);
     match cli.sub {
         SubCommand::Query(ref cmd) => {
-            do_query(&mut client, cmd).await?;
+            let result = client
+                .query(
+                    cmd.query.as_slice(),
+                    &QueryOptions::new()
+                        .env(cmd.env.as_ref())
+                        .org_id(cmd.org.clone())
+                        .build(),
+                )
+                .await?;
+            println!("Result:\n{}\n", serde_json::to_string_pretty(&result)?);
+        }
+        SubCommand::Fetch(ref cmd) => {
+            let buf = client.fetch(&cmd.path).await?;
+
+            let mut output = std::fs::File::create(&cmd.output_file)?;
+            output.write_all(&buf)?;
+            output.flush()?;
         }
     }
 
